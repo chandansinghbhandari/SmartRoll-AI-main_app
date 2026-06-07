9ae10468bdabfb12aab30c69de98f98db6ad3aff
chandansinghbhandari <chandanbhandari@gmail.com>
2026-06-08 00:27:08 +0530

    initial commit

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from src.components.dialog_auto_enroll import auto_enroll_dialog
 
 def main():
     st.set_page_config(
-        page_title='SnapClass - Making Attendance faster using AI',
-        page_icon= "https://i.ibb.co/YTYGn5qV/logo.png"
+        page_title='SmarrRoll AI - Making Attendance faster using AI',
+        page_icon= "static/img/app_logo.png"
     )
     if 'login_type' not in st.session_state:
         st.session_state['login_type'] = None
--- src/components/header.py
+++ src/components/header.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def header_home():
 
-    logo_url = "https://i.ibb.co/YTYGn5qV/logo.png"
+    logo_url = "static/img/app_logo.png"
     
     st.markdown(f"""
         <div style="display:flex; flex-direction:column; align-items:center; justify-content:center; margin-bottom:30px; margin-top:30px">
@@ -16,8 +16,7 @@ def header_home():
 
 def header_dashboard():
 
-    logo_url = "https://i.ibb.co/YTYGn5qV/logo.png"
-    
+    logo_url = "static/img/app_logo.png"
     st.markdown(f"""
         <div style="display:flex; align-items:center; justify-content:center; gap:10px">
             <img src='{logo_url}' style='height:85px;' />
