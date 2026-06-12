73901c62f658a850a1bd654a07580bb17b7b3b08
chandansinghbhandari <chandanbhandari@gmail.com>
2026-06-12 18:56:43 +0530

    reverse modify

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -13,20 +13,20 @@ def home_screen():
     col1, col2 = st.columns(2, gap="large")
 
     with col1:
-        # st.markdown(f"""
-        # <h2 style="color: black; font-weight: bold;">
-        #     I'm Student</h2> 
-        #         """, unsafe_allow_html=True)
-        st.header("I'm a Student")
+        st.markdown(f"""
+        <h2 style="color: black; font-weight: bold;">
+            I'm Student</h2> 
+                """, unsafe_allow_html=True)
+        # st.header("I'm a Student")
         st.image("https://i.ibb.co/844D9Lrt/mascot-student.png", width=120)
         if st.button('Student Portal', type='primary', icon=':material/arrow_outward:', icon_position='right'):
             st.session_state['login_type']='student'
             st.rerun()
 
     with col2:
-        # st.markdown(f"""
-        # <h2 style="color: black; font-weight: bold;">
-        #     I'm Teacher</h2> 
+        st.markdown(f"""
+        <h2 style="color: black; font-weight: bold;">
+            I'm Teacher</h2> 
         #         """, unsafe_allow_html=True)
         st.header("I'm a Teacher")
         st.image("https://i.ibb.co/CsmQQV6X/mascot-prof.png", width=145)
